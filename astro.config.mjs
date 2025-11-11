31aa05db4c60ef4ac57c9a034eff51f57d011ec5
triode-devs <triodedevs@gmail.com>
2025-11-11 16:03:32 +0530

    death squared

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
+import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server', // Enable SSR mode for dynamic routes
-  vite: {
-    plugins: [tailwindcss()],
-  }
+  output: 'hybrid',
+  adapter: cloudflare({
+    mode: 'directory', // or 'advanced' for more control
+  }),
 });
